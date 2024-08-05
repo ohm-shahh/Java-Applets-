86e82d640468deba5904f8ee0fde4490b9b9e121
ohm-shahh <shahohm66@gmail.com>
2024-08-05 19:00:14 +0530

    Create abc.java
    This code displays string in output

---
+++ abc.java
@@ -0,0 +1,14 @@
+import java.applet.*;
+import java.awt.*;
+
+public class abc extends Applet
+{
+    @Override
+    public void init()
+    {}
+    @Override
+    public void paint(Graphics g)
+    {
+        g.drawString("OHM SHAH", 50, 50);
+    }
+}
